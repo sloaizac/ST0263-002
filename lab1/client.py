b5940b1f8f777870cb1fc51ffd099a1cd654a5df
sloaizac <v-sloaiza@trueaccord.com>
2021-08-05 23:11:19 -0500

    upload lab1

---
+++ lab1/client.py
@@ -0,0 +1,103 @@
+#Client code
+"""
+August 1, 2021
+Created by: Sebastian Loaiza Correa
+"""
+#Import socket library
+import socket
+import constants
+import os
+
+#Variables for connection
+host = constants.SERVER_ADDRESS
+port = constants.PORT
+
+def runClientSocket():
+    #Create client socket
+    clientSocket = socket.socket()
+
+    #Connection with server. first parameter = IP address, second parameter = Connection Port
+    clientSocket.connect((host, port))
+    print("Connected to server")
+
+
+    #Maintain connection with the server
+    while True:
+        #Client command
+        command = input("Enter the command you want to send >> ")
+        informationToSend = ""
+        if command == constants.HELLO or command == constants.LIST_BUCKET:
+            informationToSend = command
+        elif command == constants.NEW_BUCKET:
+            bucketName = input("Enter the name of the new bucket >> ")
+            informationToSend = command+","+bucketName
+        elif command == constants.DELETE_BUCKET:
+            bucketName = input("Enter the name of the bucket to delete >> ")
+            informationToSend = command+","+bucketName
+        elif command == constants.UPLOAD_FILE:
+            filePath = input("Enter the path where the file is >> ")
+            fileName = input("Enter the name of the file to upload >> ")
+            bucketName = input("Enter the name of the bucket where the file will be uploaded >> ")
+            while True:
+                print((command+','+fileName+','+bucketName))
+                clientSocket.send((command+','+fileName+','+bucketName).encode('ascii'))
+                response = clientSocket.recv(constants.BUFFER_SIZE)
+                if response.decode('ascii') == 'ACK':
+                    f = open(filePath, "rb")
+                    while True:
+                        bytes_read = f.read(constants.BUFFER_SIZE)
+                        if not bytes_read:
+                            print('terminado')
+                            break
+                        print('send')
+                        clientSocket.sendall(bytes_read)
+                    clientSocket.send('EOF'.encode('ascii'))
+                    while True:
+                        response = clientSocket.recv(constants.BUFFER_SIZE)
+                        if response.decode('ascii') == 'OK':
+                            break
+                    break
+                else:
+                    print('Filename already exist')
+                    break
+            continue
+        elif command == constants.LIST_FILE:
+            bucketName = input("Enter the bucket name to list the files >> ")
+            informationToSend = command+","+bucketName
+        elif(command == constants.DOWNLOAD_FILE):
+            fileName = input("Enter the name of the file to download >> ")
+            bucketName = input("Enter the bucket name to download the file >> ")
+            clientSocket.send((command+','+fileName+','+bucketName).encode('ascii'))
+            f = open(os.path.join(os.getcwd(), fileName), "wb")
+            while True:
+                bytes_read = clientSocket.recv(constants.BUFFER_SIZE)
+                if bytes_read.decode('ascii')[-3:] == "EOF":
+                    f.write(bytes_read.decode('ascii')[:-3].encode('ascii'))
+                    print('terminado')
+                    break
+                print('received bites')
+                f.write(bytes_read)
+            f.close()
+            print('received complete')
+            clientSocket.send('OK'.encode('ascii'))
+            continue
+        elif(command == constants.DELETE_FILE):
+            fileName = input("Enter the name of the file to delete >> ")
+            bucketName = input("Enter the bucket name to delete the file >> ")
+            informationToSend = command+","+fileName+","+bucketName
+        elif(command == constants.EXIT):
+            break
+        else:
+            print("Non-existent command, try again")
+            continue
+        #Send message
+        clientSocket.send(bytes(informationToSend, constants.ENCODING_FORMAT))
+        #Server response
+        response = clientSocket.recv(constants.BUFFER_SIZE)
+        print(response.decode())
+    #Close connection to the server
+    clientSocket.close()
+    print("Connection closed")
+
+#Iniatialize client socket
+runClientSocket()
---
+++ lab1/server.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+"""
+August 1, 2021
+Created by: Sebastian Loaiza Correa
+"""
+
+
+import socket
+import os
+import constants
+import shutil
+import sys
+from _thread import *
+
+
+def handleConecction(client):
+	while True:
+		request = client.recv(constants.BUFFER_SIZE).decode('ascii')
+		options = request.split(',')
+		checkStatus(options)
+
+
+def checkStatus(options):
+	if options[0] == constants.NEW_BUCKET:
+		createBucket(options[1])
+	elif options[0] == constants.UPLOAD_FILE:
+		createFile(options[1], options[2])
+	elif options[0] == constants.LIST_FILE:
+		getFileList(options[1])
+	elif options[0] == constants.DELETE_FILE:
+		deleteFile(options[1], options[2])
+	elif options[0] == constants.DOWNLOAD_FILE:
+		downloadFile(options[1], options[2])
+	elif options[0] == constants.DELETE_BUCKET:
+		deleteBucket(options[1])
+	elif options[0] == constants.LIST_BUCKET or options[0] == constants.HELLO:
+		getBucketList()
+
+def downloadFile(filename, bucketName):
+	f = open(os.path.join(test_db, bucketName, filename), "rb")
+	while True:
+		bytes_read = f.read(constants.BUFFER_SIZE)
+
+		if not bytes_read:
+			break
+		print('send')
+		client.sendall(bytes_read)
+	client.send('EOF'.encode('ascii'))
+	while True:
+		response = client.recv(constants.BUFFER_SIZE)
+		if response.decode('ascii') == 'OK':
+			break
+
+def deleteFile(filename, bucketName):
+	os.remove(os.path.join(test_db, bucketName, filename))
+	client.send('file deleted successfully'.encode('ascii'))
+
+def deleteBucket(bucketName):
+	shutil.rmtree(os.path.join(test_db, bucketName))
+	client.send('bucket deleted successfully'.encode('ascii'))
+
+def createFile(filename, bucketName):
+	if os.path.exists(os.path.join(test_db, bucketName, filename)):
+		client.send('EXIST'.encode('ascii'))
+	else:
+		client.send('ACK'.encode('ascii'))
+		f = open(os.path.join(test_db, bucketName, filename), "wb")
+		while True:
+			bytes_read = client.recv(constants.BUFFER_SIZE)
+			if bytes_read.decode('ascii')[-3:] == "EOF":
+				f.write(bytes_read.decode('ascii')[:-3].encode('ascii'))
+				print('terminado')
+				break
+			print('received bites')
+			f.write(bytes_read)
+		f.close()
+		print('received-completed')
+		client.send('OK'.encode('ascii'))
+
+def getFileList(bucketName):
+	fileList = os.listdir(os.path.join(test_db, bucketName))
+	if len(fileList) > 0:
+		strList = '\n'.join([str(e) for e in fileList])
+	else:
+		strList = "Empty bucket."
+	client.send(strList.encode('ascii'))
+
+def getBucketList():
+	fileList = os.listdir(test_db)
+	if len(fileList) > 0:
+		strList = '\n'.join([str(e) for e in fileList])
+	else:
+		strList = "Zero buckets avaiables."
+	client.send(strList.encode('ascii'))
+
+def createBucket(bucketName):
+	newBucket = os.path.join(test_db, bucketName)
+	if not os.path.exists(newBucket):
+		os.makedirs(newBucket)
+		client.send('New bucket created'.encode('ascii'))
+	else:
+		client.send('Bucket name already exist'.encode('ascii'))
+
+try:
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind(("172.31.1.2", constants.PORT))
+	server.listen(5)
+	threadCount = 0
+	try:
+		test_db = sys.argv[1]
+	except:
+		test_db = input("Enter buckets path >> ")
+
+	if not os.path.exists(test_db):
+		os.makedirs(test_db)
+	print("SERVER UP!\nlistening...")
+
+	while True:
+		client, address = server.accept()
+		print('Connected to: ' + address[0] + ':' + str(address[1]))
+		start_new_thread(handleConecction, (client, ))
+		threadCount += 1
+		print('Thread Number: ' + str(threadCount))
+except Exception as error:
+	print("Server error")
+	print(error)
+
